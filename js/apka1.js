56b99364b3e516f83fd7b5dc72d19184f29675cd
Maciorex <maciej.zabski@gmail.com>
2017-11-12 14:29:00 +0100

    CardGame

--- js/apka1.js
+++ js/apka1.js
@@ -1,10 +1,50 @@
-function gameStart(){
-  
+var startButton = document.getElementById('startButton');
+var message = document.getElementById('message');
+var money = 100;
+var suits = ['spades', 'clubs', 'hearts', 'diams'];
+var numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+var cards = [];
+var cardsRand = [];
+var cardOutput = document.getElementById('cardDisplay');
+var count = 0;
+
+startButton.addEventListener('click', function () { gameStart() });
+
+
+function gameStart () {
+  message.innerHTML = 'Game Started, leeeciiimyyy';
+  startButton.style.display = 'none';
+  buildCards();
+  shuffleArray(cards);
+  cardOutput.innerHTML += cardDisplay();
 }
 
-window.onload = function(){
-  startButton = document.getElementById('startButton');
+function buildCards () {
+  cards = [];
+  for (s in suits){
+    var suit = suits[s][0].toUpperCase();
+    for (n in numbers){
+      var card = {
+        suit: suits[s],
+        number: numbers[n],
+        value: parseInt(n) + 1,
+        icon: suit
+      }
+      cards.push(card);
+    }
+  }
+}
 
-  startButton.addEventListener('click', gameStart);
+function cardDisplay () {
+  return cardsRand[count].number + ' &' + cardsRand[count].suit + ';';
+}
 
+function shuffleArray (array) {
+  for (var i = cards.length - 1; i >= 0; i--){
+    var holder = Math.floor(Math.random() * (i + 1));
+    cardsRand[i] = array[holder];
+    array.splice(holder, 1);
+    console.log(cardsRand[i]);
+  }
+  return cardsRand;
 }
